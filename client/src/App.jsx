1142f941579ee3fd5721b5c135548552da17f4e8
Arul-333 <dharu03082004@gmail.com>
2025-05-29 16:12:02 -1200

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+// import Scheduling from './pages/Scheduling';
+ import Homee from './pages/dashboard/Homee';
+// import Landing from './pages/Landing';
+//import Basic from './layouts/Basic';
+//import Reports from './pages/dashboard/Reports';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(props) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+ {/* <Basic></Basic> */}
+ {/* <Reports></Reports> */}
+ <Homee></Homee>
+    </div>
+  );
 }
+
+
 
 export default App
